d89ae3d68ef88ff85ecbd66950a539979652f3dc
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-05-06 01:12:38 +0400

    server side style sheet rendering is configured for material-ui styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,25 @@
+import { ServerStyleSheets } from "@material-ui/core";
+import * as React from "react";
 import Document, { DocumentContext } from "next/document";
 
 class MyDocument extends Document {
 	static async getInitialProps(ctx: DocumentContext) {
+		const sheets = new ServerStyleSheets();
+		const originalRenderPage = ctx.renderPage;
+		ctx.renderPage = () =>
+			originalRenderPage({
+				enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+			});
 		const initialProps = await Document.getInitialProps(ctx);
 
-		return initialProps;
+		return {
+			...initialProps,
+			// Styles fragment is rendered after the app and page rendering finish.
+			styles: [
+				...React.Children.toArray(initialProps.styles),
+				sheets.getStyleElement(),
+			],
+		};
 	}
 }
 
