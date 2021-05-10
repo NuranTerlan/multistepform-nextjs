8005ff8969d2024fcdc4f6567c18724f8edb7f83
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-05-10 22:39:05 +0400

    Custom 404 Error page is created

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+import styles from "../../styles/Error404.module.css";
+
+const CustomErrro404 = () => {
+	return <h1 className={styles.error}>Error but Success, don't worry 😂😂</h1>;
+};
+
+export default CustomErrro404;
